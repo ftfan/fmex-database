e363e6ad85e10fa288f476786143ea73edc8ddc8
imconfig <982748666@qq.com>
2020-08-30 15:10:29 +0800

    --添加usdt

--- src/bak-up.ts
+++ src/bak-up.ts
@@ -11,6 +11,12 @@ const TimeMap = {
 };
 const AliUrl = 'https://' + AppConfig.AliOss.bucket + '.' + AppConfig.AliOss.region + '.aliyuncs.com';
 
+const platformCurrencyUrl = 'https://fmex.com/api/broker/v3/zkp-assets/platform/currency';
+
+// 每个备份请求都是单线程，避免上一个未结束，就发起新的备份
+const ReqPromiseHandler: any = {};
+
+const timeout = 20000;
 /**
  * 备份基础接口数据
  */
@@ -105,10 +111,11 @@ class BakUpHandler {
         return Promise.resolve({ Url: url, Data: req.data });
       },
     },
-    // 【BTC资产信息】
+
+    // 平台的零知识证明资产备份
     {
-      OriginUrl: 'https://fmex.com/api/broker/v3/zkp-assets/platform/snapshot/BTC',
-      OssUrl: '/fmex/broker/v3/zkp-assets/platform/snapshot/BTC',
+      OriginUrl: `https://fmex.com/api/broker/v3/zkp-assets/platform/currency`,
+      OssUrl: `/fmex/api/broker/v3/zkp-assets/platform/currency`,
       Step: BakUpStep.D1,
       OssOptions: {
         headers: {
@@ -118,58 +125,26 @@ class BakUpHandler {
       CheckData: (item: BakUpData, res: any, time: Date, timeStr: string) => {
         if (!res.data) return false;
         if (res.data.status !== 0 && res.data.status !== 'ok') return false;
-        if (timeStr !== DateFormat(res.data.data.snapshot_time, TimeMap[BakUpStep.D1])) return false;
-        if (res.data.data.confirm_state !== 2) return false;
         return true;
       },
       DataFilter: (item: BakUpData, res: any, time: Date, timeStr: string) => Promise.resolve({ Url: `${item.OssUrl}/${timeStr}.json`, Data: res.data.data }),
     },
-
-    // 【用户资产信息备份】
-    {
-      CacheAble: false,
-      OriginUrl: 'https://fmex.com/api/broker/v3/zkp-assets/account/snapshot?currencyName=BTC',
-      OssUrl: '/fmex/api/broker/v3/zkp-assets/account/snapshot/BTC',
-      OssOptions: {},
-      Step: BakUpStep.D1,
-      CheckData: (item: BakUpData, res: any, time: Date, timeStr: string) => {
-        if (!res.data) return false;
-        if (res.data.status !== 0 && res.data.status !== 'ok') return false;
-        return true;
-      },
-      DataFilter: async (item: BakUpData, res: any, time: Date, timeStr: string) => {
-        // 每小时更新一次
-        const cacheKey = `${item.OssUrl}PagesCacheHash`;
-        const cahceValue = DateFormat(time, 'yyyy-MM-dd hh');
-        const cache = this.OriginCache[cacheKey];
-        if (cache && cache === cahceValue) return null;
-        const Data = [res.data.data];
-
-        const GetNextPage = async (id: string): Promise<any> => {
-          const res = await axios.get(item.OriginUrl, { params: { id } }).catch((e) => Promise.resolve(e && e.response));
-          if (!res || res.status !== 200 || !res.data || (res.data.status !== 0 && res.data.status !== 'ok')) return null;
-          Data.push(res.data.data);
-          if (res.data.data.has_next && res.data.data.next_page_id) return GetNextPage(res.data.data.next_page_id);
-          return true;
-        };
-        if (res.data.data.has_next && res.data.data.next_page_id) {
-          const success = await GetNextPage(res.data.data.next_page_id);
-          if (!success) return null;
-        }
-        const DataContent: any[] = [];
-        Data.forEach((item) => {
-          DataContent.push(...item.content);
-        });
-        this.OriginCache[cacheKey] = cahceValue;
-
-        return Promise.resolve({ Url: `${item.OssUrl}/${timeStr}.json`, Data: DataContent });
-      },
-    },
   ];
 
+  PlatformCurrency: string[] = ['btc', 'usdt'];
+  PlatformCurrencyTime = ''; // 该信息的获取时间记录
+  // 资产表备份
+  BakUpDataAssets: BakUpData[] = [];
+
   Run() {
-    this.BakUpData.forEach((item) => {
-      this.LoadAndSave(item);
+    // 设置需要保存的资产信息
+    this.BakUpDataAssets = this.GetBakUpDataAssets();
+    this.BakUpData.concat(this.BakUpDataAssets).forEach(async (item) => {
+      // 上一个请求还没处理完。
+      if (ReqPromiseHandler[item.OriginUrl]) return;
+      ReqPromiseHandler[item.OriginUrl] = this.LoadAndSave(item).then((res) => {
+        delete ReqPromiseHandler[item.OriginUrl];
+      });
       this.PackageReload(item);
     });
   }
@@ -182,10 +157,13 @@ class BakUpHandler {
     const timeStr = DateFormat(yesterday, TimeMap[BakUpStep.D1]); // yyyy-MM-dd
     const fileUrl = `${item.OssUrl}/${timeStr}.json`;
     const fileUrlToday = `${item.OssUrl}/${DateFormat(now, TimeMap[BakUpStep.D1])}.json`;
-    const cacheKey = item.OssUrl + 'yesterday';
+    const cacheKey = item.OssUrl + 'yesterday'; // 只记录昨日的缓存文件路径。这样内存不会爆炸
+
+    // 获取已经缓存的数据，如果该文件已经缓存了。就不需要后面的获取文件。
     const cache = this.OriginCache[cacheKey];
     if (cache && cache === fileUrl) return;
 
+    // 获取昨日的数据。
     const req = await axios.get(AliUrl + fileUrl).catch((e) => Promise.resolve(e && e.response));
     if (!req || req.status !== 200) return null;
 
@@ -205,24 +183,27 @@ class BakUpHandler {
     });
 
     if (!bol) return null;
+
+    // 该文件缓存成功，记录下来
     this.OriginCache[cacheKey] = fileUrl;
   }
 
   async LoadAndSave(item: BakUpData, times = 1): Promise<any> {
-    if (times >= 5) return;
+    if (times >= 5) return Promise.resolve();
     const now = new Date();
     const time = DateFormat(now, TimeMap[item.Step]);
     const cache = this.OriginCache[item.OriginUrl];
 
     const SaveUrl = `${item.OssUrl}/${time}.json`;
     if (cache === SaveUrl) return Promise.resolve('数据重复');
-    const res = await axios.get(item.OriginUrl).catch((e) => Promise.resolve(null));
+    const res = await axios.get(item.OriginUrl, { timeout }).catch((e) => Promise.resolve(null));
     if (!res || res.status !== 200) {
       console.error('LoadAndSave error', res && res.status, item.OriginUrl);
       return this.LoadAndSave(item, ++times);
     }
     if (item.CheckData) {
       const bool = item.CheckData(item, res, now, time);
+      if (bool === '数据有效') return;
       if (!bool) return this.LoadAndSave(item, ++times);
     }
 
@@ -231,7 +212,101 @@ class BakUpHandler {
     const bol = await OssClient.Save(lastData.Url, lastData.Data, item.OssOptions);
 
     if (!bol) return this.LoadAndSave(item, ++times);
+
+    // 一般数据都可以缓存。比如某个时间点获取的数据就是某个时间点的数据，但是零知识资产貌似有延迟一定时长（fmex目前是人工修改导致）
     if (item.CacheAble !== false) this.OriginCache[item.OriginUrl] = SaveUrl;
+  }
+
+  GetBakUpDataAssets() {
+    // 每小时更新一次平台资产币种
+    const timestr = DateFormat(new Date(), 'yyyy-MM-dd hh');
+    if (this.PlatformCurrencyTime !== timestr) {
+      this.PlatformCurrencyTime = timestr;
+      axios
+        .get(platformCurrencyUrl)
+        .then((res) => {
+          if (res.data && res.data.status === 'ok') {
+            this.PlatformCurrency = res.data.data;
+          }
+        })
+        .catch((e) => {
+          this.PlatformCurrencyTime = ''; // 获取失败，下次继续获取
+        });
+    }
+    const revert: BakUpData[] = [];
+    // 长度没变的情况下。默认认为资产信息没发生变化。不再重新计算资产
+    if (this.PlatformCurrency.length === this.BakUpDataAssets.length) return this.BakUpDataAssets;
+    this.PlatformCurrency.forEach((item) => {
+      const Currency = item.toLocaleUpperCase();
+      revert.push(
+        // 【BTC资产信息】
+        {
+          OriginUrl: `https://fmex.com/api/broker/v3/zkp-assets/platform/snapshot/${Currency}`,
+          OssUrl: `/fmex/broker/v3/zkp-assets/platform/snapshot/${Currency}`,
+          Step: BakUpStep.D1,
+          OssOptions: {
+            headers: {
+              'Cache-Control': AppConfig.CacheControl,
+            },
+          },
+          CheckData: (item: BakUpData, res: any, time: Date, timeStr: string) => {
+            if (!res.data) return false;
+            if (res.data.status !== 0 && res.data.status !== 'ok') return false;
+            if (timeStr !== DateFormat(res.data.data.snapshot_time, TimeMap[BakUpStep.D1])) return false;
+            if (res.data.data.confirm_state !== 2) return false;
+            return true;
+          },
+          DataFilter: (item: BakUpData, res: any, time: Date, timeStr: string) => Promise.resolve({ Url: `${item.OssUrl}/${timeStr}.json`, Data: res.data.data }),
+        },
+        // 【用户资产信息备份】
+        {
+          // 零知识资产的缓存比较特殊，所以这里特殊实现
+          CacheAble: false,
+          OriginUrl: `https://fmex.com/api/broker/v3/zkp-assets/account/snapshot?currencyName=${Currency}`,
+          OssUrl: `/fmex/api/broker/v3/zkp-assets/account/snapshot/${Currency}`,
+          OssOptions: {},
+          Step: BakUpStep.D1,
+          CheckData: (item: BakUpData, res: any, time: Date, timeStr: string) => {
+            if (!res.data) return false;
+            if (res.data.status !== 0 && res.data.status !== 'ok') return false;
+
+            const cacheKey = `${item.OssUrl}PagesCacheHash`;
+            const cahceValue = JSON.stringify(res.data); // 用第一页的数据值作为校验。一样则表示后面数据都一样
+            const cache = this.OriginCache[cacheKey];
+            if (cache && cache === cahceValue) return '数据有效';
+            return true;
+          },
+          DataFilter: async (item: BakUpData, res: any, time: Date, timeStr: string) => {
+            const cacheKey = `${item.OssUrl}PagesCacheHash`;
+            const cahceValue = JSON.stringify(res.data); // 用第一页的数据值作为校验。一样则表示后面数据都一样
+
+            const Data = [res.data.data];
+
+            const GetNextPage = async (id: string, times = 0): Promise<any> => {
+              if (times > 5) return null; // 重试次数太多了。
+              console.log('GetNextPage', Currency, id);
+              const res = await axios.get(item.OriginUrl, { params: { id }, timeout }).catch((e) => Promise.resolve(e && e.response));
+              if (!res || res.status !== 200 || !res.data || (res.data.status !== 0 && res.data.status !== 'ok')) return GetNextPage(id, ++times);
+              Data.push(res.data.data);
+              console.log('GetNextPage', Currency, id, res.data.data.content.length);
+              if (res.data.data.has_next && res.data.data.next_page_id) return GetNextPage(res.data.data.next_page_id);
+              return true;
+            };
+            if (res.data.data.has_next && res.data.data.next_page_id) {
+              const success = await GetNextPage(res.data.data.next_page_id);
+              if (!success) return null;
+            }
+            const DataContent: any[] = [];
+            Data.forEach((item) => {
+              DataContent.push(...item.content);
+            });
+            this.OriginCache[cacheKey] = cahceValue;
+            return Promise.resolve({ Url: `${item.OssUrl}/${timeStr}.json`, Data: DataContent });
+          },
+        }
+      );
+    });
+    return revert;
   }
 }
 
